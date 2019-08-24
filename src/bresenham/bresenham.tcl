f3079b6b56da49ca384465e08ffd862bb56f99c7
Luis Leon <luis.leon@ridgerun.com>
2019-08-24 10:22:48 -0600

    Improve construction system

--- src/bresenham/bresenham.tcl
+++ src/bresenham/bresenham.tcl
@@ -3,15 +3,15 @@
 ## Please DO NOT edit it.
 ## Copyright (C) 1986-2018 Xilinx, Inc. All Rights Reserved.
 ############################################################
-open_project bresenham
+open_project bresenham_accelerator
 set_top bresenham
-add_files ./bresenham/bresenham.cpp
-add_files ./bresenham/bresenham.hpp
-add_files -tb ./bresenham/bresenham_tb.cpp
+add_files bresenham.cpp
+add_files bresenham.hpp
+add_files -tb bresenham_tb.cpp
 open_solution "naive"
 set_part {xc7z020clg484-1} -tool vivado
 create_clock -period 10 -name default
-source "./bresenham/naive/directives.tcl"
+source "naive_directives.tcl"
 csim_design
 csynth_design
 cosim_design
