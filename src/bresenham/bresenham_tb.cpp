d5a6432bf026c24d6529e34dfe1e88ff28a46455
Luis Leon <luis.leon@ridgerun.com>
2019-08-24 10:06:15 -0600

    Improve testbench adding comparison against a SW version

--- src/bresenham/bresenham_tb.cpp
+++ src/bresenham/bresenham_tb.cpp
@@ -3,7 +3,11 @@
 
 #include "bresenham.hpp"
 
-#define OUTPUT_FILE "/tmp/output.raw"
+#define OUTPUT_FILE "/tmp/bresenham_line_plot.raw"
+#define X1 100
+#define X2 200
+#define Y1 2
+#define Y2 250
 
 int img_writer(interface_t img[IMG_SIZE_H][IMG_SIZE_W]) {
 	std::ofstream img_out (OUTPUT_FILE);
@@ -31,18 +35,63 @@ void init_array(interface_t img[IMG_SIZE_H][IMG_SIZE_W]) {
 	}
 }
 
+int compare_arrays(unsigned char img_sw[IMG_SIZE_H][IMG_SIZE_W],
+		interface_t img_hw[IMG_SIZE_H][IMG_SIZE_W])
+{
+	int errors = 0;
+	for(int y = 0; y < IMG_SIZE_H; y++) {
+		for(int x = 0; x < IMG_SIZE_W; x++) {
+			unsigned char hw_result = img_hw[y][x];
+			errors += img_sw[y][x] != hw_result;
+		}
+	}
+	return errors;
+}
+
+void bresenham_sw(unsigned char img[IMG_SIZE_H][IMG_SIZE_W],
+		int x1, int y1, int x2, int y2)
+{
+   int m_new = 2 * (y2 - y1);
+   int slope_error_new = m_new - (x2 - x1);
+   for (int x = x1, y = y1; x <= x2; x++)
+   {
+	  img[y][x] = 1;
+
+      /* Add slope to increment angle formed */
+      slope_error_new += m_new;
+
+      /* Slope error reached limit, time to
+       * increment y and update slope error. */
+      if (slope_error_new >= 0)
+      {
+         y++;
+         slope_error_new  -= 2 * (x2 - x1);
+      }
+   }
+}
+
 int main(int argc, char ** argv)
 {
 	/* Image buffer */
-	interface_t img[IMG_SIZE_H][IMG_SIZE_W];
-	init_array(img);
+	interface_t img_hw[IMG_SIZE_H][IMG_SIZE_W];
+	unsigned char img_sw[IMG_SIZE_H][IMG_SIZE_W] = {0};
+	init_array(img_hw);
 
-	/* Known point */
-	int x1 = 100, y1 = 2, x2 = 200, y2 = 250;
+	/* Run on HW */
+	bresenham(img_hw, X1, Y1, X2, Y2);
+	/* Run on SW */
+	bresenham_sw(img_sw, X1, Y1, X2, Y2);
 
-	bresenham(img, x1, y1, x2, y2);
+	/* Verify */
+	int errors = compare_arrays(img_sw, img_hw);
+
+	if (errors == 0) {
+		std::cout << "-- Simulation passed --" << std::endl;
+	} else {
+		std::cout << "-- Simulation failed --" << std::endl;
+	}
 
 	/* Output */
-	img_writer(img);
-	return 0;
+	img_writer(img_hw);
+	return errors;
 }
